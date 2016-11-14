2e1ef31ff9ce2a604f8c5e6b475dc01f3a58f88b
uS-aito <h2so40627@gmail.com>
2016-11-14 14:28:43 +0900

    do_GETメソッド内でリクエストを投げることに成功

--- PythonHTMLProxy.py
+++ PythonHTMLProxy.py
@@ -2,6 +2,7 @@
 import SimpleHTTPServer
 import SocketServer
 import sys
+import urllib2
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -17,6 +18,22 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         # ヘッダ取得
         splited_headers = str(self.headers).split("\r\n")
         print splited_headers
+
+        # ヘッダ整形
+        dict_header = {}
+        for header in splited_headers[:-1]:     # ヘッダ末尾の空行まで含むため
+            splited_header = header.split(": ")
+            if splited_header[0] == "Host":     # 一時的にHostヘッダを無効化
+                pass                            # localhostへのリクエストを無理やりgoogleに投げているため
+            else:
+                dict_header.update({splited_header[0]:splited_header[1]})
+        print dict_header
+
+        # ヘッダくっつけて投げる
+        url = "http://www.google.co.jp"
+        req = urllib2.Request(url=url,headers=dict_header)
+        response = urllib2.urlopen(req)
+        print response.read()
         return
 
 if __name__ == "__main__":
