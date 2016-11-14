a7dba48a846f56e757ef51eba1c516d0c2db13c4
uS-aito <h2so40627@gmail.com>
2016-11-14 14:06:03 +0900

    リクエストとヘッダを出力\nファイル名を変更

--- PythonHTMLProxy.py
+++ PythonHTMLProxy.py
@@ -1,12 +1,23 @@
+#coding: utf-8
 import SimpleHTTPServer
 import SocketServer
 import sys
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
+        # 返信を送信
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("It Works!\r\n")
+
+        # リクエスト取得
         splited_requestline = self.requestline.split()
         print splited_requestline
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+        # ヘッダ取得
+        splited_headers = str(self.headers).split("\r\n")
+        print splited_headers
+        return
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -17,4 +28,5 @@ if __name__ == "__main__":
     else:
         port = 80
     Handler = ServerHandler
+    print "Starting server in " + str(port)
     SocketServer.TCPServer(("", port), Handler).serve_forever()
